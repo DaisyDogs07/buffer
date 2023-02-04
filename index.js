9638ab75b2c9a4cec55b2d7a409e5ab3d7d5d515
DaisyDogs07 <74784459+DaisyDogs07@users.noreply.github.com>
2023-02-04 16:23:39 -0500

    A

--- index.js
+++ index.js
@@ -247,11 +247,11 @@
   function validateNumber(value, name, min, max) {
     if (typeof value !== 'number')
       throw new ERR_INVALID_ARG_TYPE(name, 'number', value);
-    if ((min != null && value < min) || (max != null && value > max) ||
-        ((min != null || max != null) && Number.isNaN(value))) {
+    if ((min && value < min) || (max && value > max) ||
+        ((min || max) && Number.isNaN(value))) {
       throw new ERR_OUT_OF_RANGE(
         name,
-        `${min != null ? `>= ${min}` : ''}${min != null && max != null ? ' && ' : ''}${max != null ? `<= ${max}` : ''}`,
+        `${min ? `>= ${min}` : ''}${min && max ? ' && ' : ''}${max ? `<= ${max}` : ''}`,
         value);
     }
   }
